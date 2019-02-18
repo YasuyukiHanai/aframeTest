8620f9aff2fe359637d2f3dc9b663d9a717ada3f
Yasuyuki Hanai <y_hanai@bbmedia.co.jp>
2019-02-18 18:30:50 +0900

    ゴーストカメラ作成

---
+++ ghost_camera/camera_cap.js
@@ -0,0 +1,103 @@
+  document.onreadystatechange = function () {
+        var canvas = document.getElementById('ghost');
+        if (canvas.getContext){
+            var ctx = canvas.getContext('2d');
+            //全体の透明度
+            ctx.globalAlpha = 0.3;
+            //1000回処理をくりかえす
+            for(i = 0; i < 1000; i++){
+                ctx.beginPath();
+                //ランダムな色を生成
+                var r = Math.floor(Math.random() * 256);
+                var g = Math.floor(Math.random() * 256);
+                var b = Math.floor(Math.random() * 256);
+                ctx.strokeStyle = 'rgb(' + r + ',' + g + ',' + b + ')';
+                //ランダムな場所に始点を移動
+                ctx.moveTo(Math.random()*400, Math.random()*300);
+                //ランダムな場所まで線を描く
+                ctx.lineTo(Math.random()*400, Math.random()*300);
+                ctx.stroke();
+            }
+        }
+    if (document.readyState === "complete") {
+      ready();
+    }
+  }
+
+  function ready() {
+    document.getElementById("capture").onclick = function() {
+          var scene = document.getElementById('ghost');
+          var video = document.getElementsByTagName('video')[0];
+
+          if (scene && video) {
+            width = video.videoWidth;
+            height = video.videoHeight;
+
+            // スクリーンショット用のcanvasを作成
+            var snapshot = document.createElement('canvas');
+            snapshot.width = width;
+            snapshot.height = height;
+            var context = snapshot.getContext('2d');
+
+            // カメラの映像をsnapshotに描画
+            context.drawImage(video, 0, 0, width, height);
+
+            // A-Frameの映像をsnapshotに描画
+            // components.screenshotの大きさを現在のwidthとheightに指定
+            scene.setAttribute('screenshot', 'width:' + width + '; height: ' + height + ';');
+            //var capture = scene.components.screenshot.getCanvas('perspective');
+            //context.drawImage(capture, 0, 0, width, height);
+
+            if (video.offsetWidth > video.offsetHeight) {
+                context.drawImage(scene, 0, 0, width, height);
+                //resizedContext.drawImage(aScene, 0, 0, width, height);
+            } else {
+                var scale = height / width;
+                var scaledWidth = height * scale;
+                var marginLeft = (width - scaledWidth) / 2;
+                context.drawImage(scene, marginLeft, 0, scaledWidth, height);
+            }
+
+
+
+            // jpgに変換してダウンロードさせる
+            // var a = document.createElement('a');
+            // a.href = snapshot.toDataURL('image/jpeg').replace('image/jpeg', 'image/octet-stream');
+            // a.download = 'aquarium.jpg';
+            // a.click();
+var imageType = "image/png";
+var fileName = "camera.png";
+var base64 = snapshot.toDataURL(imageType);
+var blob = Base64toBlob(base64);
+saveBlob(blob, fileName);
+function Base64toBlob(base64){
+    var tmp = base64.split(',');
+    var data = atob(tmp[1]);
+    var mime = tmp[0].split(':')[1].split(';')[0];
+    var buf = new Uint8Array(data.length);
+    for (var i = 0; i < data.length; i++) {
+        buf[i] = data.charCodeAt(i);
+    }
+    var blob = new Blob([buf], { type: mime });
+    return blob;
+}
+function saveBlob(blob, fileName){
+    var url = (window.URL || window.webkitURL);
+    var dataUrl = url.createObjectURL(blob);
+    var event = document.createEvent("MouseEvents");
+    event.initMouseEvent("click", true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+    var a = document.createElementNS("http://www.w3.org/1999/xhtml", "a");
+    var b = document.getElementById("capture_result");
+    // a.href = dataUrl;
+    // a.target = '_self';
+    // a.download = fileName;
+    // a.dispatchEvent(event);
+    b.classList.add("is-captured");
+    document.getElementById("capture_result_img").src = base64;
+
+    //window.open('data:' + mimeType + ';base64,' + window.Base64.encode(content), '_blank');
+}
+          }
+    };
+  }
+
