4ac13d939ac8e3e8dda8ac1f97810e4f9b9a972a
Yasuyuki Hanai <y_hanai@bbmedia.co.jp>
2019-06-05 18:44:47 +0900

    フォトフレーム再確認用

---
+++ artistarmake_frame1/js/camera_bg.js
@@ -0,0 +1,92 @@
+// Initializing camera background.
+// quated from AR.js
+// https://raw.githubusercontent.com/jeromeetienne/AR.js/master/aframe/build/aframe-ar.js
+window.addEventListener('load', function() {
+  // check API is available
+    if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices || !navigator.mediaDevices.getUserMedia) {
+    var fctName = '';
+
+    if (!navigator.mediaDevices) {
+      fctName = 'navigator.mediaDevices';
+    } else if (!navigator.mediaDevices.enumerateDevices) {
+      fctName = 'navigator.mediaDevices.enumerateDevices';
+    } else if (! navigator.mediaDevices.getUserMedia) {
+      fctName = 'navigator.mediaDevices.getUserMedia';
+    } else {
+      console.assert(false);
+    }
+
+    alert("こちらのブラウザではカメラにアクセスできません。 \n iOSをご利用の場合はSafari, Androidの場合はFirefox, Chrome, Microsoft Edgeそれぞれの最新ブラウザでお楽しみいただけます。");
+    return;
+  }
+
+  var video = document.getElementById('camera-bg');
+
+  var adjustToWindow = function (){
+    var sourceWidth = video.videoWidth;
+    var sourceHeight = video.videoHeight;
+
+    var screenWidth = window.innerWidth;
+    var screenHeight = window.innerHeight;
+
+    // compute sourceAspect
+    var sourceAspect = sourceWidth / sourceHeight
+    // compute screenAspect
+    var screenAspect = screenWidth / screenHeight
+
+    // if screenAspect < sourceAspect, then change the width, else change the height
+    if( screenAspect < sourceAspect ){
+      // compute newWidth and set .width/.marginLeft
+      var newWidth = sourceAspect * screenHeight
+      video.style.width = newWidth+'px'
+      video.style.marginLeft = -(newWidth-screenWidth)/2+'px'
+
+      // init style.height/.marginTop to normal value
+      video.style.height = screenHeight+'px'
+      video.style.marginTop = '0px'
+    }else{
+      // compute newHeight and set .height/.marginTop
+      var newHeight = 1 / (sourceAspect / screenWidth)
+      video.style.height = newHeight+'px'
+      video.style.marginTop = -(newHeight-screenHeight)/2+'px'
+
+      // init style.width/.marginLeft to normal value
+      video.style.width = screenWidth+'px'
+      video.style.marginLeft = '0px'
+    }
+  }
+
+  video.addEventListener('loadedmetadata', adjustToWindow);
+  window.addEventListener('resize', adjustToWindow);
+
+  // Start video
+  var shouldFaceUser;
+  function startinit() {
+      navigator.mediaDevices.enumerateDevices().then(function(devices) {
+        navigator.mediaDevices.getUserMedia({
+          audio: false,
+          video: {
+            facingMode: shouldFaceUser ? 'user' : 'environment',
+          }
+        }).then(function(stream) {
+          video.srcObject = stream;
+          video.play();
+        }).catch(function(error) {
+          alert("カメラの起動を拒否されました。");
+        });
+      }).catch(function(error) {
+        alert("カメラの起動を拒否されました。");
+      });
+  };
+  startinit();
+
+
+document.getElementById("flipBtn").addEventListener('click', function(){
+  video.pause()
+  video.srcObject = null
+  // toggle \ flip
+  shouldFaceUser = !shouldFaceUser;
+  startinit()
+})
+
+});
---
+++ artistarmake_frame1/js/camera_cap.js
@@ -0,0 +1,141 @@
+  document.onreadystatechange = function () {
+    if (document.readyState === "complete") {
+      ready();
+    }
+  }
+
+  function ready() {
+    document.getElementById("capture").onclick = function() {
+          var scene = document.getElementById('screen');
+          var video = document.getElementsByTagName('video')[0];
+          var loopFrame = 180;
+            var capturer = new CCapture({
+              format: 'gif',
+              //display: true,
+              quality: .7,
+              framerate: 12,
+              verbose: true,
+              name: 01,
+              workersPath: '/cosmeticpress/artistarmake_frame1/js/',
+              timeLimit: 3
+            });
+
+
+          if (scene && video) {
+            width = video.videoWidth;
+            height = video.videoHeight;
+
+            // スクリーンショット用のcanvasを作成
+            var snapshot = document.createElement('canvas');
+            snapshot.width = width;
+            snapshot.height = height;
+            var context = snapshot.getContext('2d');
+
+            var snapshotB = document.createElement('canvas');
+            snapshotB.width = width;
+            snapshotB.height = height;
+            var contextB = snapshotB.getContext('2d');
+
+            // カメラの映像をsnapshotに描画
+            context.drawImage(video, 0, 0, width, height);
+            contextB.drawImage(snapshot, 0, 0, width, height);
+
+            // A-Frameの映像をsnapshotに描画
+            // components.screenshotの大きさを現在のwidthとheightに指定
+            scene.setAttribute('screenshot', 'width:' + width + '; height: ' + height + ';');
+            //var capture = scene.components.screenshot.getCanvas('perspective');
+            //context.drawImage(capture, 0, 0, width, height);
+
+
+function render(){
+  requestAnimationFrame(render);
+  context.clearRect(0,0,width,height);
+  context.drawImage(snapshotB, 0, 0, width, height);
+  //context.drawImage(scene, 0, 0, width, height);
+  if (video.offsetWidth > video.offsetHeight) {
+      context.drawImage(scene, 0, 0, width, height);
+      //resizedContext.drawImage(aScene, 0, 0, width, height);
+  } else {
+      var scale = height / width;
+      var scaledWidth = height * scale;
+      var marginLeft = (width - scaledWidth) / 2;
+      context.drawImage(scene, marginLeft+(scaledWidth*.22), 0, (scaledWidth*.57), height);
+  }
+  capturer.capture( snapshot );
+}
+render();
+capturer.start();
+setTimeout(function() {
+  document.getElementById("capture_convert").classList.add("is-convert");
+  capturer.stop();
+  capturer.save(
+    function(blob){
+      var reader = new FileReader();
+      console.log(blob)
+      reader.readAsDataURL(blob);
+      reader.addEventListener("load", function () {
+        gifler('/cosmeticpress/artistarmake_frame1/img/gif.gif').frames('canvas.screen', onDrawFrame);
+        document.getElementById("capture_convert").classList.remove("is-convert");
+        document.getElementById("capture_result_img").src = reader.result;
+        document.getElementById("capture_result").classList.add("is-captured");
+      }, false);
+
+    }
+  );
+}, 2000);
+
+            // jpgに変換してダウンロードさせる
+            // var a = document.createElement('a');
+            // a.href = snapshot.toDataURL('image/jpeg').replace('image/jpeg', 'image/octet-stream');
+            // a.download = 'aquarium.jpg';
+            // a.click();
+// var imageType = "image/png";
+// var fileName = "camera.png";
+// var base64 = snapshot.toDataURL(imageType);
+// var blob = Base64toBlob(base64);
+
+// saveBlob(blob, fileName);
+
+// function Base64toBlob(base64){
+//     var tmp = base64.split(',');
+//     var data = atob(tmp[1]);
+//     var mime = tmp[0].split(':')[1].split(';')[0];
+//     var buf = new Uint8Array(data.length);
+//     for (var i = 0; i < data.length; i++) {
+//         buf[i] = data.charCodeAt(i);
+//     }
+//     var blob = new Blob([buf], { type: mime });
+//     return blob;
+// }
+
+// function saveBlob(blob, fileName){
+//     var url = (window.URL || window.webkitURL);
+//     var dataUrl = url.createObjectURL(blob);
+//     var event = document.createEvent("MouseEvents");
+//     event.initMouseEvent("click", true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+//     var a = document.createElementNS("http://www.w3.org/1999/xhtml", "a");
+//     var b = document.getElementById("capture_result");
+//     // a.href = dataUrl;
+//     // a.target = '_self';
+//     // a.download = fileName;
+//     // a.dispatchEvent(event);
+//     b.classList.add("is-captured");
+//     document.getElementById("capture_result_img").src = base64;
+
+//     //window.open('data:' + mimeType + ';base64,' + window.Base64.encode(content), '_blank');
+// }
+          }
+
+      var resultClose = function(){
+        document.getElementById("capture_result").classList.remove("is-captured");
+      }
+      document.getElementById("result_close").onclick = function() {
+        resultClose();
+      };
+      // document.getElementById("capture_result").onclick = function() {
+      //   resultClose();
+      // }
+
+    };
+  }
+
